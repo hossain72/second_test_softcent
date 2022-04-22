448887afc1d0983745a695245ba79b9e8a0a80db
hossain72 <hossainanowar72@gmail.com>
2022-04-23 00:14:40 +0600

    card ui design complete

--- lib/app/modules/cardPage/views/card_page_view.dart
+++ lib/app/modules/cardPage/views/card_page_view.dart
@@ -1,22 +1,98 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:second_test_softcent/app/modules/cardPage/views/widgets/transaction_widget.dart';
 
 import '../controllers/card_page_controller.dart';
 
 class CardPageView extends GetView<CardPageController> {
-  final controller = Get.put(CardPageController());
+  final CardPageController controller = Get.put(CardPageController());
+
   @override
   Widget build(BuildContext context) {
+    final height = Get.height;
+    final width = Get.width;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
-        title: Text('CardPageView'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Card',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+        ),
         centerTitle: true,
+        actions: [
+          FloatingActionButton.small(
+            onPressed: null,
+            child: Icon(Icons.add),
+            backgroundColor: Colors.green.shade400,
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'CardPageView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        height: height,
+        width: width,
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/png/card.png"),
+                          fit: BoxFit.fill)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  "Recent Transaction",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Obx(() => ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: controller.transactionList.length,
+                  itemBuilder: (_, index) {
+                    var transaction = controller.transactionList[index];
+                    return Column(
+                      children: [
+                        TransactionWidget(
+                          transaction: transaction,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        index !=
+                                controller.transactionList.length-1
+                            ? Padding(
+                              padding: const EdgeInsets.only(left: 15, right: 15),
+                              child: Divider(
+                                  height: 1,
+                                  thickness: 1.1,
+                                ),
+                            )
+                            : Container()
+                      ],
+                    );
+                  })),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/app/modules/cardPage/views/widgets/transaction_widget.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/transaction_model.dart';
+
+class TransactionWidget extends StatelessWidget {
+  final Transaction transaction;
+
+
+  const TransactionWidget({Key? key, required this.transaction})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: CircleAvatar(
+              radius: 20, // Image radius
+              backgroundImage: NetworkImage(
+                  transaction.shopLogo.toString()),
+            ),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            flex: 9,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      transaction.shopName.toString(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      transaction.timestamp.toString(),
+                      style: TextStyle(color: Colors.grey.shade600, fontSize: 14),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Trans ID : ${transaction.transactionId}",
+                      style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "- \u09F3 ${transaction.amountSend}",
+                      style: TextStyle(
+                          color: Colors.red.shade400,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        transaction.paymentType
+                            .toString() ==
+                            "Digital Payment"
+                            ? ImageIcon(AssetImage(
+                            "assets/images/png/digital_payment.png"))
+                            : ImageIcon(AssetImage(
+                            "assets/images/png/cash_payment.png")),
+                        Text(
+                          "${transaction.paymentType}",
+                          style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "+ \u09F3 ${transaction.amountRecieved}",
+                      style: TextStyle(
+                          color: Colors.green.shade400,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
